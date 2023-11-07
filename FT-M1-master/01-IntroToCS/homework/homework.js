4467a4c810a2ece23e57e1d6c393061b42e7d759
ArphaxadCR <arphaxad.calderon@gmail.com>
2023-11-07 09:00:35 -0600

    Initial Commit

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,10 +1,37 @@
-'use strict';
+"use strict";
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+  let numReversed = num.split("").reverse();
+  let aSumar = [];
+  let resultado = 0;
 
-function DecimalABinario(num) {}
+  for (let i = 0; i < numReversed.length; i++) {
+    let operacion = numReversed[i] * Math.pow(2, i);
+    aSumar.push(operacion);
+  }
+
+  for (var value of aSumar) {
+    resultado += value;
+  }
+
+  return resultado;
+}
+
+function DecimalABinario(num) {
+  let binario = [];
+
+  while (num >= 1) {
+    let residuo = num % 2;
+    binario.push(residuo);
+    num = Math.floor(num / 2);
+  }
+
+  binario.reverse();
+  let numeroBinario = binario.join("");
+  return numeroBinario;
+}
 
 module.exports = {
-   BinarioADecimal,
-   DecimalABinario,
+  BinarioADecimal,
+  DecimalABinario,
 };
